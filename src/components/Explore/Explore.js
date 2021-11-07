8a033f9c47af709a268a627f7f5e61ff36674687
Yai <arsenovlidija02@gmail.com>
2021-11-07 22:05:41 +0100

    style changes

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -11,7 +11,7 @@ const Explore = () => {
             <div className="banner-items">
               <div className="banner-search-container">
                 <div className="banner-search">
-                  <input type="search" placeholder="Search by NFT name" />
+                  <input type="text" placeholder="Search by NFT name" />
                   <div className="banner-search-icon">
                     <i className="fa fa-search"></i>
                   </div>
@@ -19,7 +19,39 @@ const Explore = () => {
                 <h4>Total Items: 51</h4>
               </div>
               <div className="filter-container">
-                <div className="switches-group"></div>
+                <div className="switches-group">
+                  <div className="filters-switch">
+                    <label className="switch">
+                      <input type="checkbox" />
+                      <span className="slider"></span>
+                    </label>
+                    <span>SolSea</span>
+                    <label className="switch">
+                      <input type="checkbox" />
+                      <span className="slider"></span>
+                    </label>
+                    <span>Verified</span>
+                    <label className="switch">
+                      <input type="checkbox" />
+                      <span className="slider"></span>
+                    </label>
+                    <span>Incl. NSFW</span>
+                  </div>
+	  <div className="filters-group">
+	  <div className="filter">
+	  <span>Sort by...</span>
+	  <div className="filter">
+	  <span>Single/Collection</span>
+	  </div>
+	  <div className="filter">
+	  <span>Filter by Tags</span>
+	  </div>
+	  <div className="filter">
+	  <span>Select Licence</span>
+	  </div>
+	  </div>
+	  </div>
+                </div>
               </div>
             </div>
           </div>
